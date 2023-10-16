21466e521f6c7a6965d6781f694e17384a099e05
tapiotorsti <torstitapi@outlook.comm>
2023-10-16 21:55:34 +0300

    Style changes header, Add-button added

--- src/components/NoteHeader.vue
+++ src/components/NoteHeader.vue
@@ -1,12 +1,22 @@
 <template>
-    <header>
-      <h1>NoteApp</h1>
-    </header>
-  </template>
+  <header>
+    <h1>NoteApp</h1>
+    <font-awesome-icon class="add-button" icon="plus" />
+  </header>
+</template>
 
 <script>
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPlus)
+
 export default {
-  name: 'NoteHeader'
+name: 'NoteHeader',
+components: {
+  FontAwesomeIcon
+}
 }
 </script>
 
